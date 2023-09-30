d1e7cdea644a18d6903fbb2d99fa9f4a77e94005
altoma <tokkto.jr@hotmail.com>
2023-09-30 16:14:47 -0500

    Add initial login page

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,26 @@
 from flask import Flask, request, jsonify
 from app.login.application.login_service import LoginService
+import os
 
-app = Flask(__name__)
+template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+template_dir = os.path.join(template_dir, 'frontend')
+template_dir = os.path.join(template_dir, 'templates')
+print(f"templates route: {template_dir}")
+
+app = Flask(__name__, template_folder=template_dir)
 login_service = LoginService()
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    req_data = request.get_json()
-    username = req_data.get('username')
-    password = req_data.get('password')
-    result = login_service.login(username, password)
-    return jsonify(result)
+
+    if request.method == 'POST':
+        req_data = request.get_json()
+        username = req_data.get('username')
+        password = req_data.get('password')
+        result = login_service.login(username, password)
+        return jsonify(result)
+        
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
