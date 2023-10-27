28cb3f66bd7f1c4d41f9785232592fb3f81e4d6d
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2023-10-27 14:54:23 -0500

    feat: add conection front to backend (login)

--- backend/login/main.py
+++ backend/login/main.py
@@ -3,8 +3,11 @@ import os
 from flask import Flask, request, jsonify, render_template
 from application.login_service import LoginService
 from infrastructure.adapters.postgres_user_repository import PostgresUserRepository
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 login_service = LoginService(PostgresUserRepository())
 
 @app.route('/', methods=['GET'])
@@ -20,8 +23,8 @@ def login():
         password = req_data.get('password')
         result = login_service.login(username, password)
         return jsonify(result)
-        
-    return render_template('login.html')
+
+    return 'Login Page'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.getenv('PORT', 5000))
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,51 +1,85 @@
 <template>
-  <img class="logo" src="../assets/logo.png" alt="Logo de CanjeXpress" />
-  <h1>Iniciar Sesión</h1>
+    <img class="logo" src="../assets/logo.png" alt="Logo de CanjeXpress" />
+    <h1>Iniciar Sesión</h1>
 
-  <div class="login">
-    <input type="text" v-model="email" placeholder="Ingresa tu correo" />
-    <input type="text" v-model="password" placeholder="Ingresa tu contraseña" />
-    <button v-on:click="login">Acceder</button>
-    <p>
-      <router-link to="/sign-up">Aun no tengo una cuenta</router-link>
-    </p>
-  </div>
+    <div class="login">
+        <input type="text" v-model="email" placeholder="Ingresa tu correo" />
+        <input type="text" v-model="password" placeholder="Ingresa tu contraseña" />
+        <button v-on:click="login">Acceder</button>
+        <p>
+            <router-link to="/sign-up">Aun no tengo una cuenta</router-link>
+        </p>
+        <p :class="{ 'hidden': !isLogin }">
+            inicio de sesión exitoso
+        </p>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'Login',
-  data() {
-    return {
-      email: '',
-      password: ''
+    name: 'Login',
+    data() {
+        return {
+            email: '',
+            password: '',
+            isLogin: false
+        }
+    },
+    methods: {
+        async login() {
+            const API_URL = import.meta.env.VITE_API_URL  + '/login'
+            try {
+                const response = await fetch(API_URL, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        username: this.email,
+                        password: this.password,
+                    }),
+                });
+
+                if (!response.ok) {
+                    throw new Error('Inicio de sesión fallido');
+                }
+
+                const data = await response.json();
+                // Procesa los datos de inicio de sesión o realiza las acciones necesarias aquí
+
+                this.isLogin = data.success;
+
+                console.log('Inicio de sesión exitoso', data);
+            } catch (error) {
+                console.error('Error al iniciar sesión:', error);
+            }
+        }
     }
-  },
-  methods: {
-    login() {
-      console.log(result)
-    }
-  }
 }
 </script>
 
 <style>
 .login input {
-  width: 200px;
-  height: 25px;
-  padding-left: 10px;
-  display: block;
-  margin-bottom: 10px;
-  margin-right: auto;
-  margin-left: auto;
-  border: 1px solid #2c3e50;
+    width: 200px;
+    height: 25px;
+    padding-left: 10px;
+    display: block;
+    margin-bottom: 10px;
+    margin-right: auto;
+    margin-left: auto;
+    border: 1px solid #2c3e50;
 }
+
 .login button {
-  width: 100px;
-  height: 22px;
-  border: 1px solid #2c3e50;
-  background: #567799;
-  color: white;
-  cursor: pointer;
+    width: 100px;
+    height: 22px;
+    border: 1px solid #2c3e50;
+    background: #567799;
+    color: white;
+    cursor: pointer;
+}
+
+.login .hidden {
+    display: none;
 }
 </style>
