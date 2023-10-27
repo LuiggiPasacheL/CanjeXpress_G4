01b74fd363c08a1a1e66f0e16a6b303bd4a674e8
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2023-10-26 23:46:58 -0500

    fix(infrastructure): Fix bulk insert data into postgresRepository

--- backend/extraccion_datos/infrastructure/adapters/postgresRepository.py
+++ backend/extraccion_datos/infrastructure/adapters/postgresRepository.py
@@ -1,9 +1,11 @@
 
-from io import TextIOWrapper
+import os
+import logging
+import psycopg2
+
 from application.ports.repository import Repository
 
-import psycopg2
-import os
+from domain.file import File
 
 class PostgresRepository(Repository):
 
@@ -18,15 +20,17 @@ class PostgresRepository(Repository):
         self.query = f"COPY {table_name} FROM %s CSV HEADER"
 
 
-    def bulkInsertData(self, data: TextIOWrapper):
+    def bulkInsertFile(self, file: File):
         """ Insert list data into database """
         cursor = self.connection.cursor()
-
+        
         try:
-            cursor.execute(self.query, (data.name,))
-            self.connection.commit()
+            data = file.read()
+            if data is not None:
+                cursor.execute(self.query, (data.name,))
+                self.connection.commit()
         except Exception as e:
-            print(e)
+            logging.error(f"Error while bulk insert data {e.__class__.__name__}")
             self.connection.rollback()
 
         cursor.close()
