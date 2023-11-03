97247b443004218f8b68e04484346f3b9b7cf1aa
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2023-11-03 08:27:01 -0500

    Add login jwt based
    - Add jwt login
    - Add LoginStatus enum
    - Add new library for jwt

--- backend/login/application/login_service.py
+++ backend/login/application/login_service.py
@@ -1,17 +1,23 @@
+from enum import Enum
 import bcrypt
 from application.ports.user_repository import UserRepository
+
+class LoginStatus(Enum):
+    OK = 1
+    BAD_CREDENTIALS = 2
+    NOT_FOUND = 3
 
 class LoginService:
     def __init__(self, user_repository: UserRepository):
         self.user_repository = user_repository
 
-    def login(self, username: str, password: str) -> dict:
+    def login(self, username: str, password: str) -> LoginStatus:
         user = self.user_repository.get_user_by_username(username)
         if user:
             is_correct = bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')) 
             if is_correct:
-                return {"success": True, "message": "OK"}
+                return LoginStatus.OK
             else:
-                return {"success": False, "message": "BADCREDENTIALS"}
+                return LoginStatus.BAD_CREDENTIALS
         else:
-            return {"success": False, "message": "NOTFOUND"}
+            return LoginStatus.NOT_FOUND
--- backend/login/main.py
+++ backend/login/main.py
@@ -1,32 +1,41 @@
 import os
 
-from flask import Flask, request, jsonify, render_template
-from application.login_service import LoginService
+from flask import Flask, request, jsonify
+from application.login_service import LoginService, LoginStatus
 from infrastructure.adapters.postgres_user_repository import PostgresUserRepository
 from flask_cors import CORS, cross_origin
+from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity, create_access_token
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'test')
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 3600
+jwt = JWTManager(app)
 
 login_service = LoginService(PostgresUserRepository())
 
-@app.route('/', methods=['GET'])
+@app.route('/validate', methods=['GET'])
+@jwt_required()
 def index():
-    return 'Hello World!'
+    current_user = get_jwt_identity()
+    return jsonify(username=current_user), 200
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST'])
 @cross_origin()
 def login():
-
     if request.method == 'POST':
         req_data = request.get_json()
         username = req_data.get('username')
         password = req_data.get('password')
         result = login_service.login(username, password)
-        return jsonify(result)
-
-    return 'Login Page with CORS enabled'
+        if result == LoginStatus.OK:
+            access_token = create_access_token(identity=username)
+            return {"success": True, "message": "OK",  "access_token": access_token}, 200
+        elif result == LoginStatus.BAD_CREDENTIALS:
+            return {"success": False, "message": "BADCREDENTIALS"}, 401
+        else:
+            return {"success": False, "message": "NOTFOUND"}, 401
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.getenv('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
