565d66fcc879466e492f4c99df980203a278b75d
francosilva25 <e.franco250299@gmail.com>
2023-11-11 00:21:57 -0500

    Micro altoma

---
+++ frontend/mf-store-products/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { ProductsComponent } from './pages/products/products.component';
+
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './pages/products/details/details.component';
+import { APP_BASE_HREF } from '@angular/common';
+
+const routes: Routes = [
+  {
+    path: 'products',
+    children:[
+      {
+        path:'',
+        component: ProductsComponent
+      },
+      {
+        path:':productId',
+        component: DetailsComponent
+      }
+
+    ]},
+  { path:'**', component: EmptyRouteComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [{provide: APP_BASE_HREF, useValue: '/'}],
+})
+export class AppRoutingModule { }
---
+++ frontend/mf-store-products/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductsModule } from './pages/products/products.module';
+import { DetailsModule } from './pages/products/details/details.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmptyRouteComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ProductsModule,
+    DetailsModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
