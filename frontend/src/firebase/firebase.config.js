a2e45458319ab9762d7ed84c7a2eaf8d193f40cb
altoma123 <tokkto.jr@hotmail.com>
2023-11-10 19:13:00 -0500

    Add react front

---
+++ frontend/src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAbNCTHwQJ1Eb2HILXQNIdbNh2KlcuG8Zs",
+    authDomain: "canjexpress-a0385.firebaseapp.com",
+    projectId: "canjexpress-a0385",
+    storageBucket: "canjexpress-a0385.appspot.com",
+    messagingSenderId: "629491886895",
+    appId: "1:629491886895:web:150183aedabd9a64bccfda",
+    measurementId: "G-0FSQEH9GXD",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
