d3d04752c619e6b50c4c3b36ef18dc51e21eef58
francosilva25 <e.franco250299@gmail.com>
2023-11-16 22:56:14 -0500

    [Frontend]: Fix navbard and cookies color

--- frontend/mf-store-navbar/src/components/navbar/navbar.component.tsx
+++ frontend/mf-store-navbar/src/components/navbar/navbar.component.tsx
@@ -1,16 +1,15 @@
-
 const Navbar = ():JSX.Element => {
   return(
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-success">
     <div className="container-fluid">
-      <a className="navbar-brand" href="/">Domini<span className="text-warning">Store</span></a>
+      <a className="navbar-brand" href="/">Canje<span className="text-warning">Xpress</span></a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/react">React</a>
+            <a className="nav-link active" aria-current="page" href="/login">Login</a>
           </li>
           <li className="nav-item">
             <a className="nav-link active" aria-current="page" href="/products">Products</a>
