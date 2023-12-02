74390da27638d75078e9c5b2a12cdb507796dde7
francosilva25 <e.franco250299@gmail.com>
2023-12-01 23:39:55 -0500

    Clear front directory

--- frontend/mf-store-related/src/root.component.tsx
+++
@@ -1,9 +0,0 @@
-import { RelatedProducts } from "./components/product/related-products";
-
-export default function Root() {
-  const pathname = window.location.pathname; 
-  const pathnameToArray = pathname.split('/');
-  const [first, path, productId] = pathnameToArray;
-
-  return (<RelatedProducts productId={productId} />);
-}
